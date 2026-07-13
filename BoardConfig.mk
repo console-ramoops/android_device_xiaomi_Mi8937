3efb4af8bb941f516e14097ec2bcb2cabec292bd
Pratik S <4ratiksharma@gmail.com>
2026-07-13 16:36:04 +0530

    Mi8937: Set TARGET_KERNEL_SOURCE to mithorium-4.19/kernel

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -46,6 +46,8 @@ DEVICE_MANIFEST_FILE += $(DEVICE_PATH)/manifest.xml
 TARGET_INIT_VENDOR_LIB := //$(DEVICE_PATH):init_xiaomi_mi8937
 
 # Kernel
+TARGET_KERNEL_CONFIG := mi8937_defconfig
+TARGET_KERNEL_SOURCE := kernel/xiaomi/mithorium-4.19/kernel
 ifeq ($(TARGET_KERNEL_VERSION),4.19)
 TARGET_KERNEL_CONFIG += \
     vendor/msm8937-legacy.config
